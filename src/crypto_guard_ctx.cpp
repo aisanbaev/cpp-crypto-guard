69bce6496290ea3b88e46c0cc3fd58ac3f35375d
isanbaev <a.isanbaev@prosoftsystems.ru>
2025-09-24 06:39:38 +0000

    ref: apply PImpl idiom to CryptoGuardCtx

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,31 @@
+#include "crypto_guard_ctx.h"
 
 namespace CryptoGuard {
+
+class CryptoGuardCtx::Impl {
+public:
+    Impl() = default;
+    ~Impl() = default;
+
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+
+    std::string CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+};
+
+CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<Impl>()) {}
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+
+// API методы
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->EncryptFile(inStream, outStream, password);
+}
+
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->DecryptFile(inStream, outStream, password);
+}
+
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream) { return pImpl_->CalculateChecksum(inStream); }
 
 }  // namespace CryptoGuard
